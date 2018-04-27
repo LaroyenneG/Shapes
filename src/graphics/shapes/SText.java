d15617c26656eeaf0500df818426fbde3bd77433
Lucas COLLOT <lucascp@hotmail.fr>
2018-04-27 21:46:05 +0200

    Filling class SText

--- src/graphics/shapes/SText.java
+++ src/graphics/shapes/SText.java
@@ -1,4 +1,45 @@
 package graphics.shapes;
 
+import java.awt.*;
+
 public class SText {
+
+    private String text;
+    private Point loc;
+
+    public SText(Point loc, String text)
+    {
+        this.loc = loc;
+        this.text = text;
+    }
+
+    public String getText()
+    {
+        return text;
+    }
+    public void setText(String text)
+    {
+        this.text = text;
+    }
+    public Point getLoc()
+    {
+        return loc;
+    }
+    public void setLoc(Point loc)
+    {
+        this.loc = loc;
+    }
+    public void translate (int dx, int dy)
+    {
+        loc.translate(dx,dy);
+    }
+    public Rectangle getBounds()
+    {
+        Rectangle rec = new Rectangle();// A modifier
+        return rec;
+    }
+    public void accept(ShapeVisitor sv)
+    {
+
+    }
 }
