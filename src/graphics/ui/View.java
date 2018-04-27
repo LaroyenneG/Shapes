86acfd64c81c1d743353cd7f58071cc3b1658fea
Lucas COLLOT <lucascp@hotmail.fr>
2018-04-27 21:07:46 +0200

    Reformat code

--- src/graphics/ui/View.java
+++ src/graphics/ui/View.java
@@ -3,41 +3,35 @@ package graphics.ui;
 import javax.swing.JPanel;
 
 
-public abstract class View extends JPanel
-{
-	private Object model;
-	private Controller controller;
+public abstract class View extends JPanel {
+    private Object model;
+    private Controller controller;
 
-	public View(Object model)
-	{
-		this.model = model;
-		this.controller = defaultController(model);
-		this.controller.setView(this);
-		this.addMouseListener(this.controller);
-		this.addMouseMotionListener(this.controller);
-		this.addKeyListener(this.controller);
-	}
-	
-	public void setModel(Object model)
-	{
-		this.model = model;
-		this.controller.setModel(model);
-	}
-	
-	public Object getModel()
-	{
-		return this.model;
-	}
-	
-	public Controller defaultController(Object model)
-	{
-		return new Controller(model);
-	}
-	
-	final public Controller getController()
-	{
-		return this.controller;
-	}
+    public View(Object model) {
+        this.model = model;
+        this.controller = defaultController(model);
+        this.controller.setView(this);
+        this.addMouseListener(this.controller);
+        this.addMouseMotionListener(this.controller);
+        this.addKeyListener(this.controller);
+    }
+
+    public void setModel(Object model) {
+        this.model = model;
+        this.controller.setModel(model);
+    }
+
+    public Object getModel() {
+        return this.model;
+    }
+
+    public Controller defaultController(Object model) {
+        return new Controller(model);
+    }
+
+    final public Controller getController() {
+        return this.controller;
+    }
 }
 
 
