aa3eac7d2a13f6d959ae587a6bc433620aacf825
laroyenneg <laroyenneg@gmail.com>
2018-04-27 22:53:17 +0200

    debug compilation

--- src/graphics/shapes/SCollection.java
+++ src/graphics/shapes/SCollection.java
@@ -1,6 +1,32 @@
 package graphics.shapes;
 
+import java.awt.*;
+
 public class SCollection extends Shape {
 
 
+    @Override
+    public Point getLoc() {
+        return null;
+    }
+
+    @Override
+    public void setLoc(Point loc) {
+
+    }
+
+    @Override
+    public void translate(int dx, int dy) {
+
+    }
+
+    @Override
+    public Rectangle getBounds() {
+        return null;
+    }
+
+    @Override
+    public void accept(ShapeVisitor sv) {
+
+    }
 }
