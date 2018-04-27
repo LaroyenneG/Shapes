36d69babc607e9f2b3f7fb88f060eb63557c6a6c
Lucas COLLOT <lucascp@hotmail.fr>
2018-04-27 22:50:26 +0200

    Filling class Shape

--- src/graphics/shapes/Shape.java
+++ src/graphics/shapes/Shape.java
@@ -1,6 +1,7 @@
 package graphics.shapes;
 
 import graphics.shapes.attributes.Attributes;
+import java.awt.*;
 
 public abstract class Shape {
 
@@ -8,7 +9,17 @@ public abstract class Shape {
     public void addAttributes(Attributes attr) {
     }
 
+   /* public Attributes getAttributes(String attr)
+    {
+
+    }*/
+
     public void add(Object r) {
     }
 
+    public abstract Point getLoc();
+    public abstract void setLoc(Point loc);
+    public abstract void translate(int dx, int dy);
+    public abstract Rectangle getBounds();
+    public abstract void accept(ShapeVisitor sv);
 }
