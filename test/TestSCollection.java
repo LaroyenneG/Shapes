c7c03ebef3a2f0f55dbe3412fe52e344192fbb76
LaroyenneG <laroyenneg@gmail.com>
2018-05-16 16:43:52 +0200

    Test SCollection empty

--- test/TestSCollection.java
+++ test/TestSCollection.java
@@ -1,5 +1,9 @@
+import graphics.shapes.SCircle;
 import graphics.shapes.SCollection;
+import graphics.shapes.SRectangle;
 import org.junit.jupiter.api.Test;
+
+import java.awt.*;
 
 public class TestSCollection {
 
@@ -8,6 +12,15 @@ public class TestSCollection {
 
 
         SCollection sCollection = new SCollection();
+        sCollection.add(new SCircle(new Point(1, 2), 3));
+        sCollection.add(new SRectangle(new Point(4, 4), 3, 6));
+
+
+        SCollection subcollection = new SCollection();
+        subcollection.add(new SRectangle(new Point(2, 1), 3, 4));
+        subcollection.add(new SRectangle(new Point(2, 3), 1, 4));
+
+        sCollection.add(subcollection);
 
         return sCollection;
     }
