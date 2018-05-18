f56bca406304739b15ce3b2f348ece66d46431fd
Bonjouravous <alexis.deckerwurtz@gmail.com>
2018-05-18 15:54:43 +0200

    Début Draftman

--- src/graphics/shapes/ui/ShapeDraftman.java
+++ src/graphics/shapes/ui/ShapeDraftman.java
@@ -6,12 +6,13 @@ import graphics.shapes.attributes.ColorAttributes;
 import java.awt.*;
 
 public class ShapeDraftman implements ShapeVisitor {
+    private Graphics2D graph2D;
 
 
     public static final ColorAttributes DEFAULT_COLOR_ATTRIBUTES = new ColorAttributes(false, false, Color.WHITE, Color.WHITE);
 
     public ShapeDraftman(Graphics g) {
-
+        graph2D = (Graphics2D) g;
     }
 
     @Override
