e21ebdb1cd02c1fe06a680d693ba7f021fbbaca7
laroyenneg <laroyenneg@gmail.com>
2018-05-19 22:11:29 +0200

    add attribute

---
+++ src/processor/shapescommands/CommandAddAttribute.java
@@ -0,0 +1,15 @@
+package processor.shapescommands;
+
+import processor.engine.Processor;
+
+public class CommandAddAttribute extends CommandShapesEditor {
+
+    public CommandAddAttribute() {
+        super("addatt");
+    }
+
+    @Override
+    public void execute(Processor processor) {
+
+    }
+}
