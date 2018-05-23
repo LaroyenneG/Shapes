8c245eda04dd4c932df138bb3e52a1030560e7b5
Alexis Decker-Wurtz <alexis.deckerwurtz@gmail.com>
2018-05-23 23:34:50 +0200

    Ajout de l'importation et de l'exporatation dans MenuController

--- src/graphics/shapes/ui/MenuController.java
+++ src/graphics/shapes/ui/MenuController.java
@@ -6,6 +6,7 @@ import graphics.ui.Controller;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.event.ActionEvent;
+import java.io.File;
 
 public class MenuController extends Controller {
 
@@ -19,6 +20,8 @@ public class MenuController extends Controller {
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
 
+        JFileChooser chooser = null;
+
         switch (actionEvent.getActionCommand()) {
 
             case EditorMenu.ID_CREDIT:
@@ -31,18 +34,37 @@ public class MenuController extends Controller {
                 break;
 
             case EditorMenu.ID_IMPORT:
-                JFileChooser chooser = new JFileChooser();
+                chooser = new JFileChooser();
                 FileNameExtensionFilter filter = new FileNameExtensionFilter(
                         "JPG & GIF Images", "jpg", "gif");
                 chooser.setFileFilter(filter);
-                if(chooser.showOpenDialog(dialog) == JFileChooser.APPROVE_OPTION) {
+                chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                if (chooser.showOpenDialog(dialog) == JFileChooser.APPROVE_OPTION) {
                     String filePath = chooser.getSelectedFile().getPath();
-                    System.out.println(filePath);
+                    File file = new File(filePath);
                 }
                 break;
 
             case EditorMenu.ID_EXPORT:
-                System.out.println("ok_export");
+                chooser = new JFileChooser();
+                chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                if (chooser.showOpenDialog(dialog) == JFileChooser.APPROVE_OPTION) {
+                    String directory = chooser.getSelectedFile().getPath();
+                    System.out.println(directory);
+
+                    String fileName = (String) JOptionPane.showInputDialog(
+                            dialog,
+                            "Input file name :",
+                            "Files",
+                            JOptionPane.PLAIN_MESSAGE,
+                            null,
+                            null,
+                            "");
+                    if(fileName != null ){
+                        File file = new File(directory+File.separator+fileName);
+                    }
+                }
+
                 break;
 
             case EditorMenu.ID_RESET:
